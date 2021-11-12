b6f84f4ea13fdfeb376af18715105a4d9caf39b4
obk0515 <93609521+obk0515@users.noreply.github.com>
2021-11-12 16:46:14 +0800

    Delete src/com/obk directory

--- src/com/obk/lei/BlackCat.java
+++
@@ -1,7 +0,0 @@
-package com.obk.lei;
-
-public class BlackCat extends Cat{
-    public BlackCat(String name, int age, String sex) {
-        super(name, age, sex, 350);
-    }
-}
--- src/com/obk/lei/CatCafe.java
+++
@@ -1,7 +0,0 @@
-package com.obk.lei;
-
-public interface CatCafe {
-    void buyCat(Cat cat) throws RuntimeException;
-    void entertainCustomer(Customer customer) throws RuntimeException;
-    void close();
-}
